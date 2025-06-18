22164673bbf78412e5a46e3eac5914f93a3b11df
juli220901 <juli220901@gmail.com>
2025-06-18 12:42:25 +0200

    create population functions

--- src/MyPackage.jl
+++ src/MyPackage.jl
@@ -5,12 +5,15 @@ include("genome.jl")
 include("create_genome.jl")
 include("forward_pass.jl")
 include("fitness.jl")
+include("create_population.jl")
 
 using .Types
 using .CreateGenome
 using .ForwardPass
 using .Fitness
+using .Population
 
-export Genome, Node, Connection, create_genome, forward_pass, evaluate_fitness
+export Genome,
+    Node, Connection, create_genome, forward_pass, evaluate_fitness, initialize_population
 
 end # module
---
+++ src/create_population.jl
@@ -0,0 +1,30 @@
+module Population
+
+using ..CreateGenome
+using ..Types
+
+export initialize_population
+
+"""
+    initialize_population(num_genomes::Int, num_inputs::Int, num_outputs::Int) → Vector{Genome}
+
+Create a population of `n` genomes initialized with given number of input/output nodes.
+Each genome is assigned a unique ID.
+
+# Arguments
+- `num_genomes`: Number of genomes to create.
+- `num_inputs`: Number of input nodes.
+- `num_outputs`: Number of output nodes.
+
+# Returns
+- A vector of `Genome` objects.
+"""
+function initialize_population(n::Int, num_inputs::Int, num_outputs::Int)
+    population = Vector{Genome}(undef, n)
+    for i in 1:n
+        population[i] = create_genome(i, num_inputs, num_outputs)
+    end
+    return population
+end
+
+end
