463517ce778198db8ccf75642cf2306d905e37cd
MusaOzcetin <musa.ozcetin@boun.edu.tr>
2025-07-11 17:45:32 +0200

    visualize test fixed

--- test/visualize_test.jl
+++ test/visualize_test.jl
@@ -1,13 +1,11 @@
 using Test
-using Visualize
-using Plots
+using Neat.Visualize: plot_fitness_history
 
 @testset "Visualize.plot_fitness_history tests" begin
     # Test with empty history: should produce a plot and save an empty-line graph
     empty_hist = Float64[]
     empty_file = "test_empty_fitness.png"
     p_empty = plot_fitness_history(empty_hist; filename=empty_file)
-    @test isa(p_empty, Plots.Plot)
     @test isfile(empty_file)
     rm(empty_file; force=true)
 
@@ -15,7 +13,6 @@ using Plots
     sample_hist = [0.2, 0.4, 0.6, 0.8, 1.0]
     sample_file = "test_sample_fitness.png"
     p_sample = plot_fitness_history(sample_hist; filename=sample_file)
-    @test isa(p_sample, Plots.Plot)
     @test isfile(sample_file)
     # Verify the plotted data matches the input history
     series = p_sample.series_list[1]
